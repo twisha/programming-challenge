7345270e90107e623b184e50cd62ef2da3c6a25d
DESAIT <DESAIT@ELSPHIL-7008184>
2019-09-10 15:14:33 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programming_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a new programming challenge!");
+        }
+    }
+}
